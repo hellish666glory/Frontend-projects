e91fcdce8e80de3732bca878ab77979ab08bd9de
Святослав Приходько <svyatos10@gmail.com>
2024-05-20 16:07:48 +0600

    buttons usage added, cookie added

--- test-work/main.js
+++ test-work/main.js
@@ -1,7 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     onScroll();
     const header = document.querySelector('.header');
-    const pageChooser = document.querySelector('.choose-page__block')
+    const pageChooser = document.querySelector('.choose-page__block');
+    const btns = document.querySelectorAll('.contact-sales-btn');
+    const cookie = document.querySelector('.cookie-block');
+    const closeCookie = document.querySelector('.close-cookie');
+    const cookieBtns = document.querySelectorAll('.cookie__btn');
+
+    cookieBtns.forEach((btn) => {
+        btn.addEventListener('mouseup', () => {
+            cookie.classList.add('visually-hidden')
+        })
+    })
+
+    cookie.classList.remove('visually-hidden');
+
+    closeCookie.addEventListener('mouseup', () => {
+        cookie.classList.add('visually-hidden');
+    })
+
+    btns.forEach((btn) => {
+        btn.addEventListener('mouseup', () => {
+            btn.classList.add('btn-loading');
+            setTimeout(() => {
+                btn.classList.remove('btn-loading')
+            }, 5000)
+        })
+    })
 
     function onScroll(){
         document.addEventListener('scroll', () => {
