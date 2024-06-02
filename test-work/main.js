1eb6851d12722ab0ddfcf4c1597d06d17c1c8c19
Святослав Приходько <svyatos10@gmail.com>
2024-06-02 14:14:06 +0600

    almost almost...

--- test-work/main.js
+++ test-work/main.js
@@ -2,18 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
     onScroll();
     const header = document.querySelector('.header');
     const pageChooser = document.querySelector('.choose-page__block');
-    const btns = document.querySelectorAll('.contact-sales-btn');
+    const btns = document.querySelectorAll('.contact-sales-btn-load');
     const cookie = document.querySelector('.cookie-block');
     const closeCookie = document.querySelector('.close-cookie');
     const cookieBtns = document.querySelectorAll('.cookie__btn');
     const burgerOpener = document.querySelector('#toggle');
-    revialElement(cookie)
+    const modal = document.querySelector('.modal');
+    const invalidText = document.createElement('span');
+    const closeModal = document.querySelector('.close-modal');
+    const modalBackdrop = document.querySelector('.modal-backdrop')
+    invalidText.classList.add('invalid-text');
+    invalidText.textContent += 'This field is required.'
+    revealElement(cookie);
+
+    closeModal.addEventListener('mouseup', () => {
+        hideElement(modal);
+        hideElement(modalBackdrop)
+    })
 
     function hideElement(elem){
         elem.classList.add('visually-hidden');
     }
 
-    function revialElement(elem){
+    function revealElement(elem){
         elem.classList.remove('visually-hidden');
     }
 
@@ -21,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (this.checked) {
             hideElement(pageChooser);
         } else {
-            revialElement(pageChooser);
+            revealElement(pageChooser);
         }
     });
 
@@ -39,8 +50,35 @@ document.addEventListener('DOMContentLoaded', () => {
         btn.addEventListener('mouseup', () => {
             btn.classList.add('btn-loading');
             setTimeout(() => {
-                btn.classList.remove('btn-loading')
-            }, 5000)
+                btn.classList.remove('btn-loading');
+                revealElement(modal);
+                revealElement(modalBackdrop);
+                const labels = document.querySelectorAll('.modal_label');
+                labels.forEach((label) => {
+                    const input = label.querySelector('input');
+                    const text = label.querySelector('.label__text')
+                    if (input.disabled){
+                        text.classList.add('disabled');
+                    } else {
+                        text.classList.remove('disabled');
+                    }
+                })
+                modal.addEventListener('submit', (e) => {
+                    e.preventDefault();
+                    const labels = document.querySelectorAll('.modal_label');
+                    labels.forEach((label) => {
+                        const input = label.querySelector('input');
+                        const alertBlock = label.querySelector('.alert-block');
+                        if (input.classList.contains('required') && input.value.trim() === ""){
+                            alertBlock.append(invalidText);
+                            input.classList.add('invalid');
+                        } if (input.classList.contains('required') && !(input.value.trim() === ""))  {
+                            input.classList.remove('invalid');
+                            invalidText.remove();
+                        }
+                    })
+                })
+            }, 2000)
         });
     });
 
@@ -51,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 hideElement(pageChooser);
                 header.classList.add('fixed');
             } else{
-                revialElement(pageChooser);
+                revealElement(pageChooser);
                 header.classList.remove('fixed');
             }
         });
