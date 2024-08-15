c5cb2675efe17628a451ad850696ee818aa445df
Святослав Приходько <svyatos10@gmail.com>
2024-08-16 01:09:42 +0600

    deleted some stuff

--- test-animation/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const flags = document.querySelectorAll(".flag");
-    flags.forEach((flag, index) => {
-      setTimeout(() => {
-        flag.style.transform = `translate(${Math.cos(2 * Math.PI * index / flags.length) * 150}px, ${Math.sin(2 * Math.PI * index / flags.length) * 150}px) rotate(${index * (360 / flags.length)}deg)`;
-      }, index * 200); // Stagger the appearance
-    });
-  });
-  
